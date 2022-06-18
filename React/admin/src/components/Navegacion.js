4c67a0a4dcb6f857d424863c017ce119f8148524
acrisanto <alanvcrisanto@gmail.com>
2022-06-18 18:12:11 -0500

    notes added

--- React/admin/src/components/Navegacion.js
+++ React/admin/src/components/Navegacion.js
@@ -1,21 +1,20 @@
 import { Navbar, Nav, Container, NavLink, NavDropdown } from "react-bootstrap";
-import {Link} from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 export default function Navegacion() {
-    return (
-        <div>
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">Administrador de Personal</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link to="/home">Home</Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    )
+	return (
+		<div>
+			<Navbar bg="light" expand="lg">
+				<Container>
+					<Navbar.Brand href="#home">Administrador de Personal</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link to="/home">Home</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</div>
+	);
 }
